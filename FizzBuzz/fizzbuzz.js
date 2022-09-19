5ee4760c688f627c1e7f1f740d470627ec620f95
Jayant Menon <email.jmenon@gmail.com>
2022-09-19 08:38:45 +0530

    passing test for 1 argument

--- FizzBuzz/fizzbuzz.js
+++ FizzBuzz/fizzbuzz.js
@@ -2,6 +2,10 @@ function fizzbuzz(limit) {
   if (typeof limit !== 'number') {
     throw new Error('limit must be a positive integer');
   }
+
+  if (arguments.length !== 1) {
+    throw new Error('accepts 1 positive integer argument');
+  }
 }
 
 module.exports = fizzbuzz;
