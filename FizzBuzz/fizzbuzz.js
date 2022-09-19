885a0406a19461b9f725e818a18133dea84520fb
Jayant Menon <email.jmenon@gmail.com>
2022-09-19 08:44:10 +0530

    passing test for positive int

--- FizzBuzz/fizzbuzz.js
+++ FizzBuzz/fizzbuzz.js
@@ -1,10 +1,14 @@
 function fizzbuzz(limit) {
+  if (arguments.length !== 1) {
+    throw new Error('accepts 1 positive integer argument');
+  }
+
   if (typeof limit !== 'number') {
     throw new Error('limit must be a positive integer');
   }
 
-  if (arguments.length !== 1) {
-    throw new Error('accepts 1 positive integer argument');
+  if (limit < 1) {
+    throw new Error('limit must be a positive integer');
   }
 }
 
